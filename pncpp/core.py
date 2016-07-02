cc24245ec9eca59a6692b0ae95f8bcb5b99dc480
Dmitry Pavliuk <dmitry.pavluk@gmail.com>
2016-07-02 12:21:48 +0300

    Fixed issued in vtable-related code

--- pncpp/core.py
+++ pncpp/core.py
@@ -101,7 +101,7 @@ def cxx_struct(name=None, virtual=0, owner=None):
         if virtual:
             vtable_type = (ctypes.c_void_p * (virtual + 4)) # ?(1), typeinfo(1), destructor(2)
             cxx_struct._vtable_ = vtable_type()
-            fields.append(('__vtable', ctypes.POINTER(vtable_type)))
+            fields.append(('_vtable_', ctypes.POINTER(vtable_type)))
         fields.extend(cxx_struct._fields_)
 
         cls_name = [cxx_struct.__name__, name][name != None]
@@ -134,6 +134,13 @@ def _new_copy(src):
 def _ptr_copy(dst_ptr, src_ptr):
     dst_ptr[0] = src_ptr[0]
 
+def _vtable_dump(vtable, max=10):
+    print("vtable at: %x" % ctypes.cast(vtable, ctypes.c_void_p).value)
+    for i, vt_entry in enumerate(vtable):
+        print "    %3d: %x" % (i, ctypes.cast(vt_entry, ctypes.c_void_p).value or 0)
+        if i > max:
+            break
+
 
 class CXXMethod(object):
 
@@ -155,7 +162,7 @@ class CXXMethod(object):
         self.c_method = None
         self.v_method = None
 
-    def _resolve_as_member(self, cls):
+    def resolve_as_member(self, cls):
         if not issubclass(cls, CXXStruct):
             raise TypeError("Can't call c++ method from non c++ class")
 
@@ -181,24 +188,28 @@ class CXXMethod(object):
 
         VMType = ctypes.CFUNCTYPE(c_method.restype, *c_method.argtypes)
 
-        self.v_method = VMType(self._py_func)
+        self.v_method = VMType(CXXClassMethodAdapter(self._py_func, cls._pyobject_))
 
-        for i, vt_entry in enumerate(cls._vtable_):
-            fptr = ctypes.cast(c_method, ctypes.c_void_p)
-            print "VTABLE RESOLVE [%d]: %x <> %x" % (i, vt_entry or 0, fptr.value)
-            if vt_entry == fptr.value:
-                print "VTABLE RESOLVED [%d]: %x == %x" % (i, vt_entry or 0, fptr.value)
-                if self.override:
-                    cls._vtable[i] = self.v_method
+        if cls._vtable_:
+            for i, vt_entry in enumerate(cls._vtable_):
+                fptr = ctypes.cast(c_method, ctypes.c_void_p)
+                #print "VTABLE RESOLVE [%d]: %x <> %x" % (i, vt_entry or 0, fptr.value)
+                if vt_entry == fptr.value:
+                    print "VTABLE RESOLVED [%d]: %x == %x" % (i, vt_entry or 0, fptr.value)
+                    if self.override:
+                        cls._vtable_[i] = ctypes.cast(self.v_method, ctypes.c_void_p)
+                        print "VTABLE OVERRIDE [%d]: %x =  %x" % (i, cls._vtable_[i] or 0, fptr.value)
 
     def __get__(self, obj, objtype=None):
         if obj is None:
             return self
         else:
-            self._resolve_as_member(objtype)
             return CXXMethodInvoke(obj, self)
 
     def __call__(self, *args):
+        if self.override:
+            return self._py_func(*args)
+
         nargs = list(args)
         for i, arg in enumerate(nargs):
             if issubclass(type(arg), CXXStruct):
@@ -211,6 +222,17 @@ class CXXMethod(object):
         result = self.c_method(*nargs)
         return result
 
+
+class CXXClassMethodAdapter(object):
+
+    def __init__(self, fn, py_object_field):
+        self._fn = fn
+        self._py_object_field = py_object_field
+
+    def __call__(self, p_struct, *args):
+        py_object_addr = getattr(p_struct.contents, self._py_object_field)
+        py_object = ctypes.cast(py_object_addr, ctypes.POINTER(ctypes.py_object)).contents.value
+        return self._fn(py_object, *args)
 
 t_void = VoidType()
 t_wchar = CXXType(ctypes.c_wchar, mg.m_wchar_t, False)
@@ -269,13 +291,19 @@ class CXXStruct(object):
     _mangled = None
     _fields_ = None
     _vtable_ = None
+    _orig_vtable_ = None
+    _pyobject_ = None
 
     @classmethod
     def link_with(cls, cdll):
         if cls._cdll == None:
             cls._cdll = cdll
-            orig_vtable = type(cls._vtable_).in_dll(cls._cdll, str(cls._mangled.vtable()))
-            cls._vtable_ = _new_copy(orig_vtable)
+            if cls._vtable_:
+                cls._orig_vtable_ = type(cls._vtable_).in_dll(cls._cdll, str(cls._mangled.vtable()))
+                cls._vtable_ = _new_copy(cls._orig_vtable_)
+            for nm, member in inspect.getmembers(cls):
+                if issubclass(type(member), CXXMethod):
+                    member.resolve_as_member(cls)
         else:
             raise Exception("Already linked with CDLL")
 
@@ -310,9 +338,24 @@ class CXXStruct(object):
         self.struct = self.CStructure()
         self.this = ctypes.pointer(self.struct)
 
+        if self._pyobject_:
+            # save pointer to python object
+            setattr(self.struct, self._pyobject_, ctypes.cast(ctypes.pointer(ctypes.py_object(self)), ctypes.c_void_p))
+
+        self.override_vtable()
+
     def override_vtable(self):
-        if hasattr(self.struct, '__vtable'):
-            self.struct.__vtable = ctypes.pointer(type(self)._vtable_)
+
+
+        if hasattr(self.struct, '_vtable_'):
+            print("override before: %x" % (ctypes.cast(self.struct._vtable_, ctypes.c_void_p).value or 0))
+            _vtable_dump(self._orig_vtable_)
+
+            new_vtable_addr = ctypes.cast(self._vtable_, ctypes.c_void_p).value + 0x10  # TODO: describe offset
+
+            self.struct._vtable_ = ctypes.cast(ctypes.c_void_p(new_vtable_addr), ctypes.POINTER(type(self._vtable_)))
+            print("override after: %x" % ctypes.cast(self.struct._vtable_, ctypes.c_void_p).value)
+            _vtable_dump(self._vtable_)
 
     def __getattr__(self, item):
         if item != "struct" and hasattr(self.struct, item):
